9474d3f16c112f6cbd6544330295b454adfa9446
bstocker <boris.stocker@gmail.com>
2025-04-03 11:04:58 +0200

    Update handler.py

--- lambda/handler.py
+++ lambda/handler.py
@@ -37,9 +37,11 @@ def handler(event, context):
             "body": json.dumps({"message": "Hello from Lambda!"})
         }
     elif route == "/contact" and method == "GET":
+        return {
             "statusCode": 200,
             "headers": {"Content-Type": "application/json"},
-            "body": json.dumps({"message": "Route /contact en get Ok"}
+            "body": json.dumps({"message": "Route /contact en get Ok"})
+        }
     elif route == "/contact" and method == "POST":
         body = json.loads(event.get("body", "{}"))
         item = {
